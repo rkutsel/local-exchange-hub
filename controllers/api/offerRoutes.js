cf68c520e70dd43bceca8b664d64544e1feef98a
Godfrey Bongomin <bongomin@uw.edu>
2022-06-06 20:39:42 -0700

    Adding newoffer.handlebars

--- controllers/api/offerRoutes.js
+++ controllers/api/offerRoutes.js
@@ -2,22 +2,34 @@ const router = require("express").Router();
 const { OfferItem } = require("../../models");
 const isAuth = require("../../utils/auth");
 
+router.post("/", isAuth, async (req, res) => {
+  try {
+    const newOffer = await OfferItem.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newOffer);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.delete("/:id", isAuth, async (req, res) => {
-    try {
-        const offerData = await OfferItem.destroy({
-          where: {
-            id: req.params.id,
-            user_id: req.session.user_id,
-          },
-        });
-    
-        if (!offerData) {
-          res.status(404).json({ message: "No offer found with this id!" });
-          return;
-        }
-    
-        res.status(200).json(postData);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+  try {
+    const offerData = await OfferItem.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!offerData) {
+      res.status(404).json({ message: "No offer found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
