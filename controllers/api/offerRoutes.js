1dd69f394f4a2483ae7a1c7bf9639bb3766f534c
Sophie Miller <millersg47@gmail.com>
2022-06-07 17:05:00 -0700

    deletes comments

--- controllers/api/offerRoutes.js
+++ controllers/api/offerRoutes.js
@@ -24,24 +24,4 @@ router.delete("/:id", isAuth, async (req, res) => {
   }
 });
 
-// router.delete("/:id", withAuth, async (req, res) => {
-//   try {
-//     const postData = await Post.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!postData) {
-//       res.status(404).json({ message: "No project found with this id!" });
-//       return;
-//     }
-
-//     res.status(200).json(postData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 module.exports = router;
